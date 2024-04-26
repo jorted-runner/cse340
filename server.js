a11698fb6ee6cb5b0654a02bfd090c3054fcd4ce
jorted-runner <jorted.runner1996@gmail.com>
2024-04-25 22:07:34 -0500

    Adding routes

--- server.js
+++ server.js
@@ -5,15 +5,27 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const env = require('dotenv').config()
 const app = express()
-const static = require("./routes/static")
+const static = require('./routes/static')
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set('view engine', 'ejs')
+app.use(expressLayouts)
+app.set('layout', './layouts/layout')
+
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+app.get('/', function(req, res) {
+  res.render('index', {title: 'Home'})
+})
 
 /* ***********************
  * Local Server Information
