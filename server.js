24ce14439dfa92cd005e121b358df30d57a027a6
jorted-runner <jorted.runner1996@gmail.com>
2024-05-07 22:32:52 -0500

    error handling

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express()
 const static = require('./routes/static')
 const baseController = require('./controllers/baseController')
 const inventoryRoute = require('./routes/inventoryRoute')
+const utilities = require('./utilities/')
 
 /* ***********************
  * View Engine and Templates
@@ -25,10 +26,14 @@ app.set('layout', './layouts/layout')
  * Routes
  *************************/
 app.use(static)
-app.get('/', baseController.buildHome)
+app.get('/', utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use('/inv', inventoryRoute)
 
+
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
 /* ***********************
 * Express Error Handler
 * Place after all other middleware
@@ -36,9 +41,10 @@ app.use('/inv', inventoryRoute)
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
   })
 })
