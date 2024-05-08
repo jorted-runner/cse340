018a2b3e3d07e4f00dc3576803c4763372b2c895
jorted-runner <jorted.runner1996@gmail.com>
2024-05-07 22:00:37 -0500

    start error handling learning activity

--- server.js
+++ server.js
@@ -28,6 +28,21 @@ app.use(static)
 app.get('/', baseController.buildHome)
 // Inventory routes
 app.use('/inv', inventoryRoute)
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
