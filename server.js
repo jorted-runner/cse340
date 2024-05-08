8f4ec617b063a56c66a9af98e5dd1eaaa66b3660
jorted-runner <jorted.runner1996@gmail.com>
2024-05-07 21:51:45 -0500

    W03 learning

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require('express-ejs-layouts')
 const env = require('dotenv').config()
 const app = express()
 const static = require('./routes/static')
+const baseController = require('./controllers/baseController')
+const inventoryRoute = require('./routes/inventoryRoute')
 
 /* ***********************
  * View Engine and Templates
@@ -23,10 +25,9 @@ app.set('layout', './layouts/layout')
  * Routes
  *************************/
 app.use(static)
-app.get('/', function(req, res) {
-  res.render('index', {title: 'Home'})
-})
-
+app.get('/', baseController.buildHome)
+// Inventory routes
+app.use('/inv', inventoryRoute)
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
