350e95c90e59a556677a6bcb74f99a7400bb6277
jorted-runner <jorted.runner1996@gmail.com>
2024-05-15 23:01:43 -0500

    password hash on register

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -2,6 +2,7 @@ const { get } = require('../routes/static')
 const utilities = require('../utilities/')
 const accountModel = require('../models/account-model')
 const validate = require('../utilities/account-validation')
+const bcrypt = require('bcryptjs')
 
 /* ****************************************
 *  Deliver login view
@@ -30,12 +31,23 @@ async function buildRegister(req, res, next) {
 async function registerAccount(req, res) {
     let nav = await utilities.getNav()
     const { account_firstname, account_lastname, account_email, account_password } = req.body
-  
+    let hashedPassword
+    try {
+      // regular password and cost (salt is generated automatically)
+      hashedPassword = await bcrypt.hashSync(account_password, 10)
+    } catch (error) {
+      req.flash("notice", 'Sorry, there was an error processing the registration.')
+      res.status(500).render("account/register", {
+        title: "Registration",
+        nav,
+        errors: null,
+      })
+    }
     const regResult = await accountModel.registerAccount(
       account_firstname,
       account_lastname,
       account_email,
-      account_password
+      hashedPassword
     )
     if (regResult) {
       req.flash(
@@ -45,12 +57,14 @@ async function registerAccount(req, res) {
       res.status(201).render("account/login", {
         title: "Login",
         nav,
+        errors: null,
       })
     } else {
       req.flash("notice", "Sorry, the registration failed.")
       res.status(501).render("account/register", {
         title: "Registration",
         nav,
+        errors: null,
       })
     }
   }
